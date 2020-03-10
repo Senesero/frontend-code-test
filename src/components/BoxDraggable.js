93a0d2b06bbc1c39185b5be3a54d9d6c6d1cc722
Senesero <senesero@gmail.com>
2020-03-10 20:04:38 +0100

    Mover caja

--- src/components/BoxDraggable.js
+++ src/components/BoxDraggable.js
@@ -1,12 +1,48 @@
 import React from "react";
 import { observer } from "mobx-react";
 import store from "../stores/MainStore";
+import interact from 'interactjs'
 
 function BoxDraggable(props) {
+  interact('.draggable')
+    .draggable({
+      // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+      listeners: {
+        move: dragMoveListener,
+      }
+    })
+
+  function dragMoveListener (event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || props.left) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || props.top) + event.dy
+
+    // translate the element
+    target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)'
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+  }
+
   return (
     <div
       id={props.id}
-      className={props.selected ? "box box-selected" : "box"}
+      className={props.selected ? "box box-selected draggable" : "box draggable"}
       style={{
         backgroundColor: props.color,
         width: props.width,
@@ -19,5 +55,6 @@ function BoxDraggable(props) {
     </div>
   );
 }
+
 
 export default observer(BoxDraggable);
